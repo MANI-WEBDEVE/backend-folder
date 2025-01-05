38a9f59c4bc38da398877376f80c4aa68972ef08
Muhammad Inam <nudmaufon@gmail.com>
2025-01-05 23:43:29 +0500

    fix: add Button component import to TodoData for improved UI functionality

--- Docker-Radis-backend/client/src/components/TodoData.tsx
+++ Docker-Radis-backend/client/src/components/TodoData.tsx
@@ -14,6 +14,7 @@ import {
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "./ui/label";
+import { Button } from "./ui/button";
 
 interface TodoDataType {
   _id?: number;
